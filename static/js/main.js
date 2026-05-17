1115184f08a3c5a30a76f4ce202453cad2860852
yatogg <valerapolatinskij203@gmail.com>
2026-05-17 21:33:01 +0900

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('contactForm');
+    if (form) {
+        form.addEventListener('submit', function (event) {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    }
+});
